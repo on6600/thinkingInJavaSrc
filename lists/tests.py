38cded3201f5d052bbaaedc1fd0ca7a25a760761
admin <824474905@qq.com>
2018-11-11 09:24:17 +0800

    Add app for lists

---
+++ lists/tests.py
@@ -0,0 +1,34 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
+from list_one.views import home_page
+from django.template.loader import render_to_string
+
+class HomePageTest(TestCase):
+	
+#	def test_root_url_resolves_to_home_page_view(self):
+#		found = resolve('/')
+#		self.assertEqual(found.func,home_page)
+	
+#	def test_home_page_returns_correct_html(self):
+#		request = HttpRequest()
+#		response = home_page(request)
+#		response = self.client.get('/')
+
+#		html = response.content.decode('utf8')
+#		expected_html = render_to_string('home.html')
+#		self.assertEqual(html,expected_html)
+#		self.assertTrue(html.startswith('<html>'))
+#		self.assertIn('<title>To-Do lists</title>',html)
+#		self.assertTrue(html.endswith('</html>'))
+		
+#		self.assertTemplateUsed(response,'wrong.html')
+		
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response,'home.html')
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/',data={'item_text':'A new list item'})
+		self.assertIn('A new list item',response.content.decode())
+		self.assertTemplateUsed(response,'home.html')
