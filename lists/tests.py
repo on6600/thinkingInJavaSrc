4dce286dd36fa137814c79b4a0b53a5eacb7a98d
admin <824474905@qq.com>
2018-11-14 22:15:39 +0800

    Model for lists Item and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -44,9 +44,9 @@ class ItemModelTest(TestCase):
 		second_item.text = 'Item the second'
 		second_item.save()
 		
-		saved_items = Item.object.all()
-		self.assertEqual(saved_item.count(0,2))
-		first_saved_item = saved_item[0]
-		second_saved_item = saved_item[1]
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text,'The first (ever) list item')
 		self.assertEqual(second_saved_item.text,'Item the second')
